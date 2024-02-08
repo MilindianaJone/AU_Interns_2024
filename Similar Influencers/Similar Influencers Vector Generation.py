e7094a3c90968abe4be989cbc36adad554f64cf3
Milind Bordia <mbordia8@gmail.com>
2024-02-08 13:22:57 -0800

    Commented Vector Generation code for TwinFluencer

--- Similar Influencers/Similar Influencers Vector Generation.py
+++ Similar Influencers/Similar Influencers Vector Generation.py
@@ -59,15 +59,13 @@ for val_guid in range(len(guid_values)):
     if index == 5: break
 
     # guid= sza
-#     print(guid)
+    # print(guid)
     TIME_PERIOD = 12 # do not change this 
     file_name = str(TIME_PERIOD)+'_'+guid+'.json'
 
     #this can be replaced by the download avro function from linqia jupyter if accessing data from aws directly
     f = open("notebook/AvroDataJson/" + guid + '.json')
     avro_data = json.load(f)
-
-
 
     social_posts = avro_data['social_posts']
     count = 0
@@ -83,7 +81,7 @@ for val_guid in range(len(guid_values)):
         
         caption = post['post_text']
 
-        
+        # Create image embedding
         media = post['media']
         imagefeatures = np.zeros(512)
         if len(media)==0: 
@@ -95,32 +93,29 @@ for val_guid in range(len(guid_values)):
             media_count = media[i]
 
             image_url = media_count['url']
-
-            
+           
             try:
               response = requests.get(image_url)
               image = Image.open(BytesIO(response.content))
             except:
-              continue
- 
-
+              continue 
 
             image1 = preprocess(image).unsqueeze(0).to(device)
             
             imagefeatures += model.encode_image(image1).detach().numpy().flatten()
             image_count += 1
-
             
         if image_count > 0: imagefeatures = imagefeatures/image_count
 
         count2 += 1
 
-
         try:
+            # Limit of 77 characters per line when doing embeddings for emojis
             context_length = 77
 
             blocks = [caption[i:i+context_length] for i in range(0, len(caption), context_length)]
             
+            # Word embedding
             embeddings = [clip.tokenize(block, context_length=context_length).to(device) for block in blocks]
             
 
@@ -130,8 +125,6 @@ for val_guid in range(len(guid_values)):
         textfeaturesall = [model.encode_text(embeds) for embeds in embeddings]
         textfeatures = torch.stack(textfeaturesall).mean(dim=0).to(device)
         count += 1
-
-
         
         stacked = np.hstack([imagefeatures,np.mean(textfeatures.detach().numpy(),axis=0).flatten()])
 
@@ -145,12 +138,9 @@ for val_guid in range(len(guid_values)):
     influencer_embedding[len(influencer_embedding)//2:] /= count
     embeds.append(influencer_embedding)
 
-
+    # Add vector to pinecone
     pineconeindex.upsert([{"id":guid,"values":influencer_embedding.tolist()}])
     
-
-
-
 
 first_guids = guid_values[:index-1]
 
